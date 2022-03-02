ee0ab09bc86ebd46d09b3b532c184fef52619d44
muhammadeko <muhammadekoprasetyo29@gmail.com>
2022-03-02 16:11:28 +0700

    change seed admin

--- evote-web/app/Database/Seeds/admin.php
+++ evote-web/app/Database/Seeds/admin.php
@@ -13,7 +13,7 @@ class Admin extends Seeder
 			'kode_admin' => 'ADM0001',
 			'username_admin' => 'evoteci',
 			'email_admin' => 'muhammadeko.if@gmail.com',
-			'password_admin' => '243279243130242f587a566a6831354a31473451667832426e4c45704f5851685436456c4f4852796b732e626732342f6c43322f754355424f567171'
+			'password_admin' => password_hash(env("DEFAULT_ADMIN_PASSWORD"),PASSWORD_DEFAULT)
 		]);
 	}
 }
