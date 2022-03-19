5ced447c271d0ccda596a68fef706da14e9a25bc
Novandi Kevin Pratama <kevinpret27@gmail.com>
2022-03-19 10:17:07 +0700

    Meta Apple

--- evote-web/app/Views/v2/user/Pilih.php
+++ evote-web/app/Views/v2/user/Pilih.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
     <title>Pemilihan</title>
     <link rel="shortcut icon" href="<?= base_url('img/logox.ico') ?>">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
